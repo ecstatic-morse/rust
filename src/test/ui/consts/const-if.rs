d2154a6dc06e21620f3e79c9568f751e12f9dcb5
Dylan MacKenzie <ecstaticmorse@gmail.com>
2019-09-19 11:26:50 -0700

    Add feature flag to existing const if test

--- src/test/ui/consts/const-if.rs
+++ src/test/ui/consts/const-if.rs
@@ -1,5 +1,5 @@
+#![feature(const_if_match)]
+
 const _X: i32 = if true { 5 } else { 6 };
-//~^ ERROR constant contains unimplemented expression type
-//~| ERROR constant contains unimplemented expression type
 
 fn main() {}
