6209a14f4b65632a4ff4e464d883733e1c528687
Dylan MacKenzie <ecstaticmorse@gmail.com>
2019-09-19 12:38:43 -0700

    Extend const-if test

--- src/test/ui/consts/const-if.rs
+++ src/test/ui/consts/const-if.rs
@@ -1,5 +1,11 @@
+// run-pass
 #![feature(const_if_match)]
 
-const _X: i32 = if true { 5 } else { 6 };
+const _: i32 = if true { 5 } else { 6 };
+
+const _: i32 = match 6 {
+    x @ 5 | x @ 6 | x @ 7 => x,
+    _ => 0
+};
 
 fn main() {}
